bbed6fad10fb821c1bde42a906843fc57a3cba0e
MohammedBadry <mahamedbadry2003@gmail.com>
2022-03-17 18:51:03 +0200

    First commit

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ notification }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeComponent",
+        data() {
+            return {
+                notification: ""
+            }
+        },
+        mounted() {
+            window.Echo.channel('Notify').listen('NotificationTest', event => {
+                this.notification = event.msg
+            })
+        }
+    }
+</script>
